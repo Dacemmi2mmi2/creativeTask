3bb59f07c82cfc6cdcf7ff95e4774bd2ef047f92
Dacemmi2mmi2 <46452261+Dacemmi2mmi2@users.noreply.github.com>
2021-03-21 07:34:04 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+const htmlElements = {
+    main : document.querySelector('.main'),
+    leftSlide : document.querySelector('.leftSlide'),
+    rightSlide : document.querySelector('.rightSlide'),
+    imgNew : document.querySelector('.imgNew'),
+    titleOfTea : document.querySelector('.titleOfTea'),
+    imgTitle : document.querySelector('.imgTitle'),
+    imgsStrawberry : document.querySelectorAll('.strawberry'),
+    imgsPeach : document.querySelectorAll('.peach'),
+    allBackgroungImg : document.querySelectorAll('.bgi'),
+    imgsRaspberry : document.querySelectorAll('.raspberry'),
+    wrapperSlider : document.querySelector('.wrapperSlider'),
+    itemsSlider : document.querySelectorAll('.item'),
+    link : document.querySelector('.google'),
+}
+
+
+const showContent = function showContentFirstLoad() {
+    setTimeout(() => {
+        htmlElements.titleOfTea.style.top = 0;
+        htmlElements.wrapperSlider.style.left = 0;
+    }, 500);
+
+    setTimeout(() => {
+        htmlElements.imgTitle.style.transform = 'scale(' + 1 + ')';
+    }, 900);
+
+    setTimeout(() => {
+        htmlElements.imgNew.style.top = 0;
+    }, 1400);
+
+    setTimeout(() => {
+        htmlElements.imgsStrawberry.forEach(item => {
+            item.style.transform = 'scale(' + 1 + ')';
+        })
+    }, 10);
+    
+    setTimeout(() => {
+        htmlElements.leftSlide.style.transform = 'scale(' + 1 + ')';
+        htmlElements.rightSlide.style.transform = 'scale(' + 1 + ')';
+        htmlElements.link.style.opacity = 1;
+    }, 1900);
+}
+showContent();
+
+
+const showImg =  function showBgiForSlide(param) {
+    htmlElements.allBackgroungImg.forEach(item => {
+        if(item.classList[0] === param){
+            item.style.opacity = 1;
+            item.style.transform = 'scale(' + 1 + ')';
+        }else{
+            item.style.opacity = 0;
+            item.style.transform = 'scale(' + 0 + ')';
+        }
+    });
+}
+
+
+const leftSlide = function showLeftSlide() {
+    htmlElements.itemsSlider.forEach(item => {
+        if(item.offsetLeft < 0){
+            item.style.opacity = 1;
+            item.style.left = 0;
+            showImg(item.classList[1]);
+        }
+        if(item.offsetLeft === 0){
+            item.style.left = 100 + '%';
+            item.style.opacity = 0;
+        }
+        if(item.offsetLeft > 0){
+            item.style.left = -100 + '%';
+            item.style.opacity = 0;
+        }
+    });
+}
+
+
+const rightSlide = function showRightSlide() {
+    htmlElements.itemsSlider.forEach(item => {
+        if(item.offsetLeft > 0){
+            item.style.opacity = 1;
+            item.style.left = 0;
+            showImg(item.classList[1]);
+        }
+        if(item.offsetLeft === 0){
+            item.style.left = -100 + '%';
+            item.style.opacity = 0;
+        }
+        if(item.offsetLeft < 0){
+            item.style.left = 100 + '%';
+            item.style.opacity = 0;
+        }
+    });
+}
+
+
+htmlElements.main.addEventListener('click', event => {
+    event.target.closest('.rightSlide') ? rightSlide() : null;
+    event.target.closest('.leftSlide') ? leftSlide() : null;
+});
